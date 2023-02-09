731a5be68d554380132a109df4b717103cf48a5e
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2023-02-09 15:55:44 -0700

    converted website to nextjs
    converted website to nextjs pogman

---
+++ vortexvoyages/src/components/Home.jsx
@@ -0,0 +1,21 @@
+import '@/styles/Home.css'
+import NavBar from '@/components/NavBar'
+
+export default function Home() {
+	return (
+		<div className="App">
+			<NavBar></NavBar>
+			<div className="App-header">
+				<h1 className="App-heading">SPAAAAAAACE</h1>
+				<div className="App-text">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+						tenetur, vel architecto numquam laborum tempora molestiae vitae
+						praesentium nemo qui unde minus? Culpa nobis doloribus esse sit,
+						assumenda recusandae praesentium.
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ vortexvoyages/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import '../styles/NavBar.css'
+
+export default function NavBar() {
+	return (
+		<div className="NavBar">
+			<div className="NavBar-logo">
+				<img src="./img/planet.png" alt="Planet logo" />
+			</div>
+			<div className="NavBar-links">
+				<div className="NavBar-link">
+					<a href="#">Pricing</a>
+				</div>
+				<div className="NavBar-link">
+					<a href="#">Updates</a>
+				</div>
+				<div className="NavBar-link">
+					<a href="#">Launches</a>
+				</div>
+				<div className="NavBar-link">
+					<a href="#">Contact</a>
+				</div>
+			</div>
+		</div>
+	)
+}
