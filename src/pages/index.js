b292852817dc4dba76c6b936bce3578e18daaf4d
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2023-02-14 19:53:38 -0700

    reformatted and renamed the repository
    reformatted and renamed the repository so that it can be better organized and easier to work with

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from '@/components/Head'
+import Home from '@/components/Home'
+
+export default function HomePage() {
+  return (
+    <>
+      <Head title="Vortex Voyages"></Head>
+      <Home></Home>
+    </>
+  )
+}
