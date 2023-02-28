f23a103a169cad55d98134a95fa03a59c7a30caf
Alespren <corbinped@gmail.com>
2023-02-27 22:23:32 -0700

    Update landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,15 @@ export default function Home() {
 	return (
 		<div className="App">
 			<NavBar></NavBar>
-			<div className="App-header">
-				<h1 className="App-heading">SPAAAAAAACE</h1>
-				<div className="App-text">
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-						tenetur, vel architecto numquam laborum tempora molestiae vitae
-						praesentium nemo qui unde minus? Culpa nobis doloribus esse sit,
-						assumenda recusandae praesentium.
-					</p>
+			<div className="App-main">
+				<div className="App-wrapper">
+					<h1 className="App-heading">The Final Frontier</h1>
+					<div className="App-text">
+						<p>
+							Take a trip into the next step of human exploration, and
+							experience something unlike anything else
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
