89684b92741ddb50cb73288b4dff45eb722e57b6
Alespren <corbinped@gmail.com>
2023-03-07 13:06:56 -0700

    Added table to pricing page

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -1,30 +1,47 @@
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Pricing.module.css'
 import NavBar from '@/components/NavBar'
 import Spacer from '@/components/Spacer'
-import Section from '@/components/Section'
 
 export default function Pricing() {
 	return (
 		<div className={styles.App}>
 			<NavBar></NavBar>
-			<div className={styles.AppMain}>
-				<div className={styles.AppWrapper}>
-					<h1 className={styles.AppHeading}>Testing testing</h1>
-					<div className={styles.AppText}>
-						<p>
-							Take a trip into the next step of human exploration, and
-							experience something unlike anything else
-						</p>
+			<Spacer></Spacer>
+			<Spacer></Spacer>
+			<div className={styles.PricingHeader}>Pricing Plans</div>
+			<div className={styles.PricingWrapper}>
+				<div className={styles.PricingColumns}>
+					<div className={styles.PricingColumn}>
+						<h2>Economy</h2>
+						<h3>$10,000</h3>
+						<ul>
+							<li>Comfortable Seating</li>
+							<li>Expert Tour Guide</li>
+							<li>Lorem ipsum</li>
+							<li>Dolor sit amet.</li>
+						</ul>
+					</div>
+					<div className={styles.PricingColumn}>
+						<h2>Buisness</h2>
+						<h3>$10,000</h3>
+						<ul>
+							<li>Comfortable Seating</li>
+							<li>Expert Tour Guide</li>
+							<li>Lorem ipsum</li>
+							<li>Dolor sit amet.</li>
+						</ul>
+					</div>
+					<div className={styles.PricingColumn}>
+						<h2>First Class</h2>
+						<h3>$10,000</h3>
+						<ul>
+							<li>Comfortable Seating</li>
+							<li>Expert Tour Guide</li>
+							<li>Lorem ipsum</li>
+							<li>Dolor sit amet.</li>
+						</ul>
 					</div>
 				</div>
-			</div>
-			<Spacer></Spacer>
-			<Section></Section>
-			<Spacer></Spacer>
-			<Spacer></Spacer>
-			<div className={styles.AppPurchase}>
-				<h1 className={styles.PurchaseHeader}>Join the waitlist today</h1>
-				<button className={styles.button}>Register</button>
 			</div>
 		</div>
 	)
