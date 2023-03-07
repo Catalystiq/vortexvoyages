69b81b2e77b30a01e0e1a4200e3edbe8f6e6ed41
Alespren <corbinped@gmail.com>
2023-03-07 08:48:35 -0700

    Increase spacer

--- src/components/Spacer.jsx
+++ src/components/Spacer.jsx
@@ -1,5 +1,5 @@
 import styles from '@/styles/Spacers.module.css'
 
 export default function Spacer() {
-	return <div className={styles.Spacer20}></div>
+	return <div className={styles.Spacer50}></div>
 }
