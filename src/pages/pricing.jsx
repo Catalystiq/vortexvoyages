f6939ef43a029ec01171de67ee2407e1a4154db8
Alespren <corbinped@gmail.com>
2023-03-07 16:58:04 -0700

    more text

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -51,13 +51,34 @@ export default function Pricing() {
 					</div>
 				</div>
 				<Spacer></Spacer>
-				<div className={styles.PricingText}>
-					<h1>Lorem, ipsum dolor.</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-						ducimus, aut, enim a illo autem amet deserunt ratione sequi, eum
-						assumenda vero? Voluptates, ratione distinctio.
-					</p>
+				<Spacer></Spacer>
+				<div className={styles.PricingTextWrapper}>
+					<div className={styles.PricingText}>
+						<h1>Lorem, ipsum dolor.</h1>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+							ducimus, aut, enim a illo autem amet deserunt ratione sequi, eum
+							assumenda vero? Voluptates, ratione distinctio.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+							ducimus, aut, enim a illo autem amet deserunt
+						</p>
+					</div>
+					<Spacer></Spacer>
+					<Spacer></Spacer>
+					<div className={styles.PricingText}>
+						<h1>Lorem, ipsum dolor.</h1>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+							ducimus, aut, enim a illo autem amet deserunt ratione sequi, eum
+							assumenda vero? Voluptates, ratione distinctio.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+							ducimus, aut, enim a illo autem amet deserunt
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
