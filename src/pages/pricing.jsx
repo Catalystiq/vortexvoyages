ac7153c6d6b60065bdb80eadf09c4d2b5d78c692
Alespren <corbinped@gmail.com>
2023-03-07 15:43:50 -0700

    add text below pricing

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -4,43 +4,60 @@ import Spacer from '@/components/Spacer'
 
 export default function Pricing() {
 	return (
-		<div className={styles.App}>
+		<div className={styles.Pricing}>
 			<NavBar></NavBar>
-			<Spacer></Spacer>
-			<Spacer></Spacer>
-			<div className={styles.PricingHeader}>Pricing Plans</div>
 			<div className={styles.PricingWrapper}>
-				<div className={styles.PricingColumns}>
-					<div className={styles.PricingColumn}>
-						<h2>Economy</h2>
-						<h3>$10,000</h3>
-						<ul>
-							<li>Comfortable Seating</li>
-							<li>Expert Tour Guide</li>
-							<li>Lorem ipsum</li>
-							<li>Dolor sit amet.</li>
-						</ul>
+				<Spacer></Spacer>
+				<Spacer></Spacer>
+				<div className={styles.PricingHeader}>Pricing Plans</div>
+				<div className={styles.PricingTable}>
+					<div className={styles.PricingColumns}>
+						<div className={styles.PricingColumn}>
+							<h2>Economy</h2>
+							<h3>$10,000</h3>
+							<ul>
+								<li>Comfortable Seating</li>
+								<li>Expert Tour Guide</li>
+								<li>Lorem ipsum</li>
+								<li>Dolor sit amet.</li>
+								<li>Lorem ipsum</li>
+								<li>Dolor sit amet.</li>
+							</ul>
+						</div>
+						<div className={styles.PricingColumn}>
+							<h2>Buisness</h2>
+							<h3>$10,000</h3>
+							<ul>
+								<li>Comfortable Seating</li>
+								<li>Expert Tour Guide</li>
+								<li>Lorem ipsum</li>
+								<li>Dolor sit amet.</li>
+								<li>Lorem ipsum</li>
+								<li>Dolor sit amet.</li>
+							</ul>
+						</div>
+						<div className={styles.PricingColumn}>
+							<h2>First Class</h2>
+							<h3>$10,000</h3>
+							<ul>
+								<li>Comfortable Seating</li>
+								<li>Expert Tour Guide</li>
+								<li>Lorem ipsum</li>
+								<li>Dolor sit amet.</li>
+								<li>Lorem ipsum</li>
+								<li>Dolor sit amet.</li>
+							</ul>
+						</div>
 					</div>
-					<div className={styles.PricingColumn}>
-						<h2>Buisness</h2>
-						<h3>$10,000</h3>
-						<ul>
-							<li>Comfortable Seating</li>
-							<li>Expert Tour Guide</li>
-							<li>Lorem ipsum</li>
-							<li>Dolor sit amet.</li>
-						</ul>
-					</div>
-					<div className={styles.PricingColumn}>
-						<h2>First Class</h2>
-						<h3>$10,000</h3>
-						<ul>
-							<li>Comfortable Seating</li>
-							<li>Expert Tour Guide</li>
-							<li>Lorem ipsum</li>
-							<li>Dolor sit amet.</li>
-						</ul>
-					</div>
+				</div>
+				<Spacer></Spacer>
+				<div className={styles.PricingText}>
+					<h1>Lorem, ipsum dolor.</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+						ducimus, aut, enim a illo autem amet deserunt ratione sequi, eum
+						assumenda vero? Voluptates, ratione distinctio.
+					</p>
 				</div>
 			</div>
 		</div>
